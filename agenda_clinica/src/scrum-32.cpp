1e571dbb226a5d4d3ff3f7943c62abe305394981
Miguel Lama Del Valle <i32lavam@uco.es>
2014-12-04 14:32:21 +0100

    v2

--- agenda_clinica/src/scrum-32.cpp
+++ agenda_clinica/src/scrum-32.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 int main() {
-	cout << "Hello crazys" << endl; // prints !!!Hello World!!!
+	cout << "Hello oliii" << endl; // prints !!!Hello World!!!
 	cout << "Hola locos" << endl; // prints !!!Hello World!!!
 	return 0;
 
