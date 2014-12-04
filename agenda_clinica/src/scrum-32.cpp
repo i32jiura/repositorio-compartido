fbb7c57fd15308a7f44962612ccdc9aece1020d4
i32jiura <i32jiura@uco.es>
2014-12-04 13:43:44 +0100

    Clases 1 commit

---
+++ agenda_clinica/src/scrum-32.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : scrum-32.cpp
+// Author      : scrum32
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
