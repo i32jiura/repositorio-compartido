3a34ec0079190d496d394cdb7b2ca854948a2eba
Jesus Rio Barrilero <jesusriobarrilero@gmail.com>
2015-01-07 13:41:41 +0100

    Importar Copia de Seguridad -refactorizada-

--- agenda_clinica/src/Agenda.cpp
+++ agenda_clinica/src/Agenda.cpp
@@ -37,47 +37,46 @@ void Agenda::importarCopia() {
 	cout << "Introduce la ruta del archivo a importar: ";
 	cin >> rutaArchivo;
 
-	// Abrimos los ficheros.
+	// Abrimos el fichero de la copia de seguridad que se pasa como parametro
 	ficheroCopia.open(rutaArchivo.c_str());
 
+	// Comprobamos que el archivo se ha abierto correctamente
 	if (!ficheroCopia) {
-		cout
-				<< "Ocurrió un error cuando se intentaba acceder a la agenda modo lectura."
-				<< endl;
-		//exit(-1);
+		cout << "Ocurrió un error cuando se intentaba acceder a la copia de seguridad." << endl;
+		exit(-1);
 	}
 
+	// Abrimos el fichero de la agenda en modo escritura, para escribir al fianl del fichero
 	ficheroAgendaW.open("Agenda.txt", ofstream::app);
 
+	// Comprobamos que el archivo se ha abierto correctamente
 	if (!ficheroAgendaW) {
-		cout
-				<< "Ocurrió un error cuando se intentaba acceder a la agenda modo escritura."
-				<< endl;
-		//exit(-1);
+		cout << "Ocurrió un error cuando se intentaba acceder a la agenda modo escritura." << endl;
+		exit(-1);
 	}
 
 	// Recorremos los ficheros para encontrar duplucados e insertarlos.
 	while (getline(ficheroCopia, lineaCopia, '\n')) {
 
+		// Obtenemos el DNI del archivo de la copia de seguridad para compararlo
 		DNI = lineaCopia.substr(0, 9);
 
-		cout << "FICHERO COPIA = " + DNI + "\n";
-
+		// Abrimos el archivo de la Agenda en modo lectura para comparar
 		ficheroAgendaR.open("Agenda.txt");
 
+		// Comprobamos que el archivo se ha abierto correctamente
 		if (!ficheroAgendaR) {
-			cout
-					<< "Ocurrió un error cuando se intentaba acceder al archivo de copia de seguridad."
-					<< endl;
-			//exit(-1);
+			cout << "Ocurrió un error cuando se intentaba acceder a la agenda." << endl;
+			exit(-1);
 		}
 
+		// Recorremos el fichero de la agenda para comparar con los datos de la copia de seguridad.
 		while (getline(ficheroAgendaR, lineaAgenda, '\n')) {
 
+			// Obtenemos el DNI del archivo de la agenda para compararlo
 			DENI = lineaAgenda.substr(0, 9);
 
-			cout << "FICHERO AGENDA = " + DENI + "\n";
-
+			// Si el DNI ha aparecido, ponemos el auxiliar a 0 para no introducir
 			if ((DNI.compare(DENI)) == 0) {
 
 				aux = 0;
@@ -85,14 +84,21 @@ void Agenda::importarCopia() {
 
 		}
 
+		// Cerramos el fichero de agenda en modo lectura.
 		ficheroAgendaR.close();
 
+		// Si el DNI no ha aparecido aux deberia ser 1 y se introduce al final del fichero.
 		if (aux == 1) {
 			ficheroAgendaW << "\n";
 			ficheroAgendaW << lineaCopia.c_str();
 		}
 
+		// Ponemos aux a 1 para la siguiente vuelta del bucle.
 		aux = 1;
 	}
+
+	// Cerramos los ficheros de agenda y copia de seguridad
+	ficheroCopia.close();
+	ficheroAgendaW.close();
 
 }
