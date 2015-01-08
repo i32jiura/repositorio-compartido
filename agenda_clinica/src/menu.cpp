2d47d958684d91538671b8191febd044ba8ae6d9
i32gopic <cgpino34@gmail.com>
2015-01-08 14:27:54 +0100

    menu actualizado

--- agenda_clinica/src/menu.cpp
+++ agenda_clinica/src/menu.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main () {
 
-	int opc;
+	int opc1,opc2;
+	char aux;
 	Agenda a;
 	Pacientes p;
 	string cadena;
@@ -25,13 +26,13 @@ int main () {
 		cout << "\n * 6.-Mostrar lista de pacientes                   *";
 		cout << "\n * 7.-Mostrar lista de frencuentes                 *";
 		cout << "\n * 8.-Mostrar lista de favoritos                   *";
-		cout << "\n * 9.-Importar copia de seguridad                  *";
+		cout << "\n * 9.-Importar copia de seguridad                 *";
 		cout << "\n * 10.-Exportar copia de seguridad                 *";
 		cout << "\n * 0.-Salir de la aplicación                       *";
 		cout << "\n ***************************************************\n >> ";
-		cin >> opc;
+		cin >> opc1;
 		
-		switch(opc) {
+		switch(opc1) {
 		
 			case 1:
 				cout << "\n # Rellena los siguientes campos para el nuevo cliente:\n";	
@@ -53,7 +54,7 @@ int main () {
 				getline(cin,cadena);
 				p.setTelefono(cadena);
 				
-				a.introducir(p);
+				a.introducirPaciente(p);
 
 				cout << "\n # Nuevo cliente añadido con éxito\n # Pulsa Intro para continuar";
 				getchar();
@@ -63,7 +64,6 @@ int main () {
 			
 				cout << "\n # Introduce los apellidos del cliente que deseas borrar\n -> ";
 				getline(cin,cadena);
-				
 				
 				if (a.borrarPaciente(cadena) == true) {
 				
@@ -77,8 +77,80 @@ int main () {
 				getchar();
 				break;
 			case 3:
+				getchar();
+			
+				cout << "\n # Introduce los apellidos del cliente que deseas buscar\n -> ";
+				getline(cin,cadena);
+
+				/* Se busca al cliente en la lista y se devuelve la información si se encuentra*/
 				break;
 			case 4:
+				getchar();
+			
+				cout << "\n # Introduce los apellidos del cliente que deseas modificar\n -> ";
+				getline(cin,cadena);
+
+
+
+				/* Se busca al cliente en la lista y se devuelve la información si se encuentra*/
+
+				do {
+
+					cout << "\n # Indica qué miembro quieres actualizar del paciente:\n 1.-Nombre\n 2.-Apellidos\n 3.-Dni\n 4.-Teléfonos\n 5.-Correos\n 6.-Notas\n 7.-Añadir/Quitar favoritos\n 0.-Salir y Sobrescribir datos -> ";
+					cin >> opc2;
+
+					switch(opc2) {
+
+						case 1:
+							cout << "\n # Introduce nuevo Nombre -> ";
+							getline(cin,cadena);
+							p.setNombre(cadena);
+							break;
+						case 2:
+							cout << "\n # Introduce nuevos Apellidos -> ";
+							getline(cin,cadena);
+							p.setApellidos(cadena);
+							break;
+						case 3:
+							cout << "\n # Introduce nuevo Dni -> ";
+							getline(cin,cadena);
+							p.setDni(cadena);
+							break;
+						case 4:
+							cout << "\n # Introduce nuevos Teléfonos -> ";
+							getline(cin,cadena);
+							p.setTelefonos(cadena);
+							break;
+						case 5:
+							cout << "\n # Introduce nuevos Correos -> ";
+							getline(cin,cadena);
+							p.setCorreos(cadena);
+							break;
+						case 6:
+							cout << "\n # Introduce nuevas Notas -> ";
+							getline(cin,cadena);
+							p.setNombre(cadena);
+							break;
+						case 7:
+							cout << "\n # ¿Marcar como favorito? s/n -> ";
+							cin >> aux;
+
+							if (aux == 's') {
+
+								setFavorito(true);
+							}else{
+								setFavorito(false);
+							}
+							break;
+					}
+				}while(opc2 != 0);
+
+				/* Función de modificar */
+
+				cout << "\n # Datos modificados con éxito\n # Pulsa Intro para continuar";
+				getchar();
+				getchar();
+
 				break;
 			case 5:	
 				break;
@@ -92,6 +164,8 @@ int main () {
 				break;
 			case 10:		
 				break;
+			case 11:		
+				break;
 			case 0:
 				cout << "\n # Saliendo de la aplicación...\n";
 				break;
@@ -102,7 +176,7 @@ int main () {
 				getchar();
 				break;
 		}
-	}while(opc != 0);
+	}while(opc1 != 0);
 
 	return 0;
 }
